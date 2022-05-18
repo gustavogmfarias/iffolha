fe9b6135ae5aff62fba3357ef0e90599b193500a
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-05-18 15:01:54 -0300

    .

---
+++ services/api.ts
@@ -0,0 +1,98 @@
+import axios, { AxiosError } from "axios";
+import { Router } from "next/router";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContext";
+import { AuthTokenError } from "./errors/AuthTokenError";
+
+let isRefreshing = false;
+let failedRequestQueue = [];
+
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: "http://localhost:5000",
+    headers: {
+      Authorization: `Bearer ${cookies["nextauth.token"]}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response.status === 401) {
+        if (error.response.data?.code === "token.expired") {
+          cookies = parseCookies(ctx);
+
+          const { "nextauth.refreshToken": refreshToken } = cookies;
+
+          const originalConfig = error.config;
+
+          if (!isRefreshing) {
+            isRefreshing = true;
+            api
+              .post("/refreshToken", { refreshToken })
+              .then((response) => {
+                const { token } = response.data;
+
+                setCookie(ctx, "nextauth.token", token, {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                });
+
+                setCookie(
+                  ctx,
+                  "nextauth.refreshToken",
+                  response.data.refreshToken,
+                  {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: "/",
+                  }
+                );
+
+                api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+                failedRequestQueue.forEach((request) =>
+                  request.onSucess(token)
+                );
+                failedRequestQueue = [];
+              })
+              .catch((err) => {
+                failedRequestQueue.forEach((request) => request.onFailure(err));
+                failedRequestQueue = [];
+
+                if (typeof window !== "undefined") {
+                  signOut();
+                }
+              })
+              .finally(() => {
+                isRefreshing = false;
+              });
+          }
+          return new Promise((resolve, reject) => {
+            failedRequestQueue.push({
+              onSucess: (token: string) => {
+                originalConfig.headers["Authorization"] = `Bearer ${token}`;
+                resolve(api(originalConfig));
+              },
+              onFailure: (err: AxiosError) => {
+                reject(err);
+              },
+            });
+          });
+        } else {
+          if (typeof window !== "undefined") {
+            signOut();
+          } else {
+            return Promise.reject(new AuthTokenError());
+          }
+        }
+      }
+
+      return Promise.reject(error);
+    }
+  );
+  return api;
+}
---
+++ utils/withSSRAuth.ts
@@ -0,0 +1,58 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type WithSSRAuthOptions = { permissions?: string[]; roles?: string[] };
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+) {
+  //fn: GetServerSideProps significa que a função vai receber como parametro uma outra função
+
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx); //ctx é o contexto, no caso, next page context
+    const token = cookies["nextauth.token"];
+
+    if (!token)
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return { redirect: { destination: "/dashboard", permanent: false } };
+      }
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, "nextauth.token");
+        destroyCookie(ctx, "nextauth.refreshToken");
+        return { redirect: { destination: "/", permanent: false } };
+      }
+    }
+  };
+}
